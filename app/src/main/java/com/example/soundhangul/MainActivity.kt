6ea79e70bea04bf22c93144235974e6bdf544d8a
cristalcho <cristalcho@unist.ac.kr>
2020-01-11 14:59:07 +0900

    Main Activity

--- app/src/main/java/com/example/soundhangul/MainActivity.kt
+++ app/src/main/java/com/example/soundhangul/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.example.soundhangul
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var go_consonant = findViewById(R.id.consonant) as Button
+
+        go_consonant.setOnClickListener{
+            var nextConsonant = Intent(this, PracticeConsonant::class.java)
+            startActivity(nextConsonant)
+        }
     }
 
 
-    consonant.setOnClickListener{
-    	var nextConsonant = Intent(this, PracticeConsonant::class.java)
-	startActivity(nextConsonant)
-    }
+
 }
