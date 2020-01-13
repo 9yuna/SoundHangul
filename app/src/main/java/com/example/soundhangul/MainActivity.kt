5cdd94c7e40f520decdf6d8ec5c1c9f81b9937e1
9yuna <9yuna.b@gmail.com>
2020-01-14 00:38:19 +0900

    add PracticeVowel imgView, btn

--- app/src/main/java/com/example/soundhangul/MainActivity.kt
+++ app/src/main/java/com/example/soundhangul/MainActivity.kt
@@ -6,28 +6,23 @@ import android.os.Bundle
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var go_consonant = findViewById(R.id.consonant) as Button
-
-        go_consonant.setOnClickListener{
-            var nextConsonant = Intent(this, PracticeConsonant::class.java)
+        consonant.setOnClickListener{
+            val nextConsonant = Intent(this@MainActivity, PracticeConsonant::class.java)
             startActivity(nextConsonant)
         }
 
-        var go_vowel = findViewById(R.id.vowel) as Button
-
-        go_vowel.setOnClickListener{
-            var nextVowel = Intent(this, PracticeVowel::class.java)
+        vowel.setOnClickListener{
+            val nextVowel = Intent(this@MainActivity, PracticeVowel::class.java)
             startActivity(nextVowel)
         }
 
     }
-
-
 
 }
--- app/src/main/java/com/example/soundhangul/PracticeVowel.kt
+++ app/src/main/java/com/example/soundhangul/PracticeVowel.kt
@@ -2,11 +2,56 @@ package com.example.soundhangul
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_practice_vowel.*
+import java.util.*
+
 
 class PracticeVowel : AppCompatActivity() {
+
+    val vowel_list = listOf("","ㅏ","ㅑ","ㅓ","ㅕ","ㅗ","ㅛ","ㅜ","ㅠ","ㅡ","","ㅐ","ㅒ","ㅔ","ㅔ","ㅘ","ㅙ","ㅚ","ㅝ","ㅞ","ㅟ","ㅢ")
+    var current_num = 1
+
+    fun setImgBnt(currnet: Int){
+        var file_name: String = "mo"+currnet
+        var file_id: Int = resources.getIdentifier(file_name, "drawable", "com.example.soundhangul")
+        problem_img.setImageDrawable(resources.getDrawable(file_id))
+
+        var cnt: Int = 0
+        var setting_done = mutableListOf<Boolean>(false, false, false)
+
+        while(cnt < 3) {
+            var random: Int = Random().nextInt(setting_done.size)
+            var vowel_random: Int = Random().nextInt(vowel_list.size) + 1
+
+            if(setting_done[random] == false) {
+                setting_done[random] = true
+                cnt++
+                when (random) {
+                    0 -> {
+                        answer1_btn.setText(vowel_list[vowel_random])
+                    }
+                    1 -> {
+                        answer2_btn.setText(vowel_list[vowel_random])
+                    }
+                    2 -> {
+                        answer3_btn.setText(vowel_list[currnet])
+                    }
+
+                }
+            }
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_practice_vowel)
 
+        current_num = 1
+        setImgBnt(current_num)
+
+
+
     }
+
 }
