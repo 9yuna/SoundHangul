1b5079763a4b1c852c19abf972843a797f0c389c
cristalcho <cristalcho@unist.ac.kr>
2020-01-29 23:03:36 +0900

    sequencial consonant practice

--- app/src/main/java/com/example/soundhangul/PracticeConsonant.kt
+++ app/src/main/java/com/example/soundhangul/PracticeConsonant.kt
@@ -1,6 +1,10 @@
 package com.example.soundhangul
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class PracticeConsonant : AppCompatActivity() {
@@ -9,5 +13,17 @@ class PracticeConsonant : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_practice_consonant)
 
+        var go_sequence = findViewById(R.id.sequence) as Button
+        go_sequence.setOnClickListener {
+            var nextSequence = Intent(this, SequenceConsonant::class.java)
+            startActivity(nextSequence)
+        }
+
+        var go_random = findViewById(R.id.random) as Button
+        go_random.setOnClickListener {
+            var nextRandom = Intent(this, SequenceConsonant::class.java)
+            startActivity(nextRandom)
+        }
     }
+
 }
