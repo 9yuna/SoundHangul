1dec3deabaf3080907551885a0a6a277ca7596d0
cristalcho <cristalcho@unist.ac.kr>
2020-02-08 12:46:49 +0900

    Add Kakao TTS at Consonant

--- app/src/main/java/com/example/soundhangul/MainActivity.kt
+++ app/src/main/java/com/example/soundhangul/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.soundhangul
 
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import android.widget.Button
+import com.kakao.util.helper.Utility.getPackageInfo
 import kotlinx.android.synthetic.main.activity_main.*
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +20,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var mContext = applicationContext
+        var key = getKeyHash(mContext)
+        Log.d("Key", "HashKey:"+key)
 
         consonant.setOnClickListener{
             val nextConsonant = Intent(this@MainActivity, PracticeConsonant::class.java)
@@ -22,7 +34,24 @@ class MainActivity : AppCompatActivity() {
             val nextVowel = Intent(this@MainActivity, PracticeVowel::class.java)
             startActivity(nextVowel)
         }
-
     }
 
+    fun getKeyHash(mcon:Context) : String? {
+        val packageInfo = getPackageInfo(mcon, PackageManager.GET_SIGNATURES)
+        if (packageInfo == null)
+            return null
+        for (signature in packageInfo.signatures)
+        {
+            try
+            {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                return Base64.encodeToString(md.digest(), Base64.NO_WRAP)
+            }
+            catch (e: NoSuchAlgorithmException) {
+                Log.w("main", "Unable to get MessageDigest. signature=" + signature, e)
+            }
+        }
+        return null
+    }
 }
--- app/src/main/java/com/example/soundhangul/RandomConsonant.kt
+++ app/src/main/java/com/example/soundhangul/RandomConsonant.kt
@@ -1,5 +1,6 @@
 package com.example.soundhangul
 
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
@@ -7,13 +8,23 @@ import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.kakao.sdk.newtoneapi.SpeechRecognizerManager
+import com.kakao.sdk.newtoneapi.TextToSpeechClient
+import com.kakao.sdk.newtoneapi.TextToSpeechListener
+import com.kakao.sdk.newtoneapi.TextToSpeechManager
 import java.util.*
 
 class RandomConsonant : AppCompatActivity() {
     var ja = arrayOf("ㄱ", "ㄴ", "ㄷ", "ㄹ", "ㅁ", "ㅂ", "ㅅ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ")
     var str = arrayOf("기역", "니은", "디귿", "리을", "미음", "비읍", "시옷", "이응", "지읒", "치읒", "키읔", "티긑", "피흪", "히흫")
     var rnum = intArrayOf(0, 0, 0, 0)
+
+    //for tts
+    val TAG = "Kakao"
+    var ttsClient : TextToSpeechClient? = null
+    val NETWORK_STATE_CODE = 0
 
     fun findImage(i: Int): Int{
         var s = "ja" + i.toString()
@@ -85,12 +96,38 @@ class RandomConsonant : AppCompatActivity() {
         var jaumNum:Int = rnd.nextInt(14)
         var nextJaumNum:Int = 987654321
 
+        SpeechRecognizerManager.getInstance().initializeLibrary(this)
+        TextToSpeechManager.getInstance().initializeLibrary(this)
+        ttsClient = TextToSpeechClient.Builder()
+            .setSpeechMode(TextToSpeechClient.NEWTONE_TALK_1)     // 음성합성방식
+            .setSpeechSpeed(1.0)            // 발음 속도(0.5~4.0)
+            .setSpeechVoice(TextToSpeechClient.VOICE_WOMAN_READ_CALM)  //TTS 음색 모드 설정(여성 차분한 낭독체)
+            .setListener(object : TextToSpeechListener {
+                //아래 두개의 메소드만 구현해 주면 된다. 음성합성이 종료될 때 호출된다.
+                override fun onFinished() {
+                    val intSentSize = ttsClient?.getSentDataSize()      //세션 중에 전송한 데이터 사이즈
+                    val intRecvSize = ttsClient?.getReceivedDataSize()  //세션 중에 전송받은 데이터 사이즈
+
+                    val strInacctiveText = "handleFinished() SentSize : $intSentSize  RecvSize : $intRecvSize"
+
+                    Log.i(TAG, strInacctiveText)
+                }
+
+                override fun onError(code: Int, message: String?) {
+                    Log.d(TAG, code.toString())
+                }
+            })
+            .build()
+        //----------
+
         // 이미지랑 발음 보여주기
         setNextJaum(bigJaum, pronunciation, btn1, btn2, btn3, btn4, jaumNum)
+        ttsClient?.play(str[jaumNum])
         Log.d("### I: ", "$jaumNum")
         // 올바른 버튼 클릭시 다음 자음으로 넘어가기
         btn1.setOnClickListener{
             if(jaumNum == rnum[0]){
+                ttsClient?.play(str[rnum[0]])
                 nextJaumNum = rnd.nextInt(14)
                 while(nextJaumNum == jaumNum){
                     nextJaumNum = rnd.nextInt(14)
@@ -100,11 +137,14 @@ class RandomConsonant : AppCompatActivity() {
                 jaumNum = nextJaumNum
 
                 Log.d("### I(1): ", "$jaumNum")
+                Thread.sleep(1000L)
                 setNextJaum(bigJaum, pronunciation, btn1, btn2, btn3, btn4, jaumNum)
+                Thread.sleep(1000L)
+                ttsClient?.play(str[jaumNum])
             }else{
                 Log.d("###not I(1): ", "$jaumNum")
                 btn1.setTextColor(Color.RED)
-
+                ttsClient?.play(str[rnum[0]])
                 Handler().postDelayed({
                     btn1.setTextColor(Color.BLACK)
                 }, 200)
@@ -113,6 +153,7 @@ class RandomConsonant : AppCompatActivity() {
 
         btn2.setOnClickListener{
             if(jaumNum == rnum[1]){
+                ttsClient?.play(str[rnum[1]])
                 nextJaumNum = rnd.nextInt(14)
                 while(nextJaumNum == jaumNum){
                     nextJaumNum = rnd.nextInt(14)
@@ -120,13 +161,15 @@ class RandomConsonant : AppCompatActivity() {
                     Log.d("while(nextJaumNum): ", "$nextJaumNum")
                 }
                 jaumNum = nextJaumNum
-
+                Thread.sleep(1000L)
                 Log.d("### I(2): ", "$jaumNum")
                 setNextJaum(bigJaum, pronunciation, btn1, btn2, btn3, btn4, jaumNum)
+                Thread.sleep(1000L)
+                ttsClient?.play(str[jaumNum])
             }else{
                 Log.d("###not I(2): ", "$jaumNum")
                 btn2.setTextColor(Color.RED)
-
+                ttsClient?.play(str[rnum[1]])
                 Handler().postDelayed({
                     btn2.setTextColor(Color.BLACK)
                 }, 200)
@@ -134,6 +177,7 @@ class RandomConsonant : AppCompatActivity() {
         }
         btn3.setOnClickListener{
             if(jaumNum == rnum[2]){
+                ttsClient?.play(str[rnum[2]])
                 nextJaumNum = rnd.nextInt(14)
                 while(nextJaumNum == jaumNum){
                     nextJaumNum = rnd.nextInt(14)
@@ -141,13 +185,15 @@ class RandomConsonant : AppCompatActivity() {
                     Log.d("while(nextJaumNum): ", "$nextJaumNum")
                 }
                 jaumNum = nextJaumNum
-
+                Thread.sleep(1000L)
                 Log.d("### I(3): ", "$jaumNum")
                 setNextJaum(bigJaum, pronunciation, btn1, btn2, btn3, btn4, jaumNum)
+                Thread.sleep(1000L)
+                ttsClient?.play(str[jaumNum])
             }else{
                 Log.d("###not I(3): ", "$jaumNum")
                 btn3.setTextColor(Color.RED)
-
+                ttsClient?.play(str[rnum[2]])
                 Handler().postDelayed({
                     btn3.setTextColor(Color.BLACK)
                 }, 200)
@@ -155,6 +201,7 @@ class RandomConsonant : AppCompatActivity() {
         }
         btn4.setOnClickListener{
             if(jaumNum == rnum[3]){
+                ttsClient?.play(str[rnum[3]])
                 nextJaumNum = rnd.nextInt(14)
                 while(nextJaumNum == jaumNum){
                     nextJaumNum = rnd.nextInt(14)
@@ -162,17 +209,36 @@ class RandomConsonant : AppCompatActivity() {
                     Log.d("while(nextJaumNum): ", "$nextJaumNum")
                 }
                 jaumNum = nextJaumNum
-
+                Thread.sleep(1000L)
                 Log.d("### I(4): ", "$jaumNum")
                 setNextJaum(bigJaum, pronunciation, btn1, btn2, btn3, btn4, jaumNum)
+                Thread.sleep(1000L)
+                ttsClient?.play(str[jaumNum])
             }else{
                 Log.d("###not I(4): ", "$jaumNum")
                 btn4.setTextColor(Color.RED)
-
+                ttsClient?.play(str[rnum[3]])
                 Handler().postDelayed({
                     btn4.setTextColor(Color.BLACK)
                 }, 200)
             }
         }
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            NETWORK_STATE_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        TextToSpeechManager.getInstance().finalizeLibrary()
     }
 }
