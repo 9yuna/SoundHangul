492f094eed8a088b0685080e5c723b62836c3c6a
cristalcho <cristalcho@unist.ac.kr>
2020-02-21 22:15:43 +0900

    SoundWord resolve

--- app/src/main/java/com/example/soundhangul/JaFragment1.kt
+++ app/src/main/java/com/example/soundhangul/JaFragment1.kt
@@ -4,6 +4,7 @@ package com.example.soundhangul
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -16,12 +17,13 @@ import com.kakao.sdk.newtoneapi.TextToSpeechClient
 import com.kakao.sdk.newtoneapi.TextToSpeechListener
 import com.kakao.sdk.newtoneapi.TextToSpeechManager
 import kotlinx.android.synthetic.main.fragment_ja_fragment1.*
+import java.util.*
 
 class JaFragment1 : Fragment() {
-
+    var tts: TextToSpeech? = null
     //for tts
     val TAG = "Kakao"
-    var ttsClient : TextToSpeechClient? = null
+ //   var ttsClient : TextToSpeechClient? = null
     val NETWORK_STATE_CODE = 0
 
     override fun onCreateView(
@@ -42,98 +44,75 @@ class JaFragment1 : Fragment() {
         var Ja9 = view!!.findViewById<View>(R.id.paBtn8) as Button
         var Ja10 = view!!.findViewById<View>(R.id.paBtn9) as Button
 
-        SpeechRecognizerManager.getInstance().initializeLibrary(App.instance.context())
-        TextToSpeechManager.getInstance().initializeLibrary(App.instance.context())
-        ttsClient = TextToSpeechClient.Builder()
-            .setSpeechMode(TextToSpeechClient.NEWTONE_TALK_1)     // 음성합성방식
-            .setSpeechSpeed(1.0)            // 발음 속도(0.5~4.0)
-            .setSpeechVoice(TextToSpeechClient.VOICE_WOMAN_READ_CALM)  //TTS 음색 모드 설정(여성 차분한 낭독체)
-            .setListener(object : TextToSpeechListener {
-                //아래 두개의 메소드만 구현해 주면 된다. 음성합성이 종료될 때 호출된다.
-                override fun onFinished() {
-                    val intSentSize = ttsClient?.getSentDataSize()      //세션 중에 전송한 데이터 사이즈
-                    val intRecvSize = ttsClient?.getReceivedDataSize()  //세션 중에 전송받은 데이터 사이즈
-
-                    val strInacctiveText = "handleFinished() SentSize : $intSentSize  RecvSize : $intRecvSize"
-
-                    Log.i(TAG, strInacctiveText)
+        tts = TextToSpeech(activity, TextToSpeech.OnInitListener{
+            if(it == TextToSpeech.SUCCESS) {
+                val result: Int = tts!!.setLanguage(Locale.KOREA)
+                //언어 데이터가 없거나 혹은 언어가 지원하지 않으면…
+                //언어 데이터가 없거나 혹은 언어가 지원하지 않으면…
+                if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    Toast.makeText(activity, "이 언어는 지원하지 않습니다.", Toast.LENGTH_SHORT).show()
+                } else {
+                    //음성 톤
+                    tts!!.setPitch(0.7f)
+                    //읽는 속도
+                    tts!!.setSpeechRate(1.2f)
                 }
-
-                override fun onError(code: Int, message: String?) {
-                    Log.d(TAG, code.toString())
-                }
-            })
-            .build()
-
+            }
+        })
 
         Ja1.setOnClickListener {
             Log.d("button", Ja1.text.toString())
-            ttsClient?.play(Ja1.text.toString())
-            SoundCard().btnclick(Ja1.text.toString())
+            //    ttsClient?.play(Ja1.text.toString())
+            tts!!.speak(Ja1.text.toString(), TextToSpeech.QUEUE_FLUSH, null,null)
+            // Log.d("btnClick", tmp)
         }
         Ja2.setOnClickListener {
             Log.d("button", Ja2.text.toString())
-            ttsClient?.play(Ja2.text.toString())
-            SoundCard().btnclick(Ja2.text.toString())
+        //    ttsClient?.play(Ja2.text.toString())
+            tts!!.speak(Ja2.text.toString(), TextToSpeech.QUEUE_FLUSH, null,null)
         }
         Ja3.setOnClickListener {
             Log.d("button", Ja3.text.toString())
-            ttsClient?.play(Ja3.text.toString())
-            SoundCard().btnclick(Ja3.text.toString())
+            tts!!.speak(Ja3.text.toString(), TextToSpeech.QUEUE_FLUSH, null,null)
+       //     ttsClient?.play(Ja3.text.toString())
         }
         Ja4.setOnClickListener {
             Log.d("button", Ja4.text.toString())
-            ttsClient?.play(Ja4.text.toString())
-            SoundCard().btnclick(Ja4.text.toString())
+        //    ttsClient?.play(Ja4.text.toString())
+
         }
         Ja5.setOnClickListener {
             Log.d("button", Ja5.text.toString())
-            ttsClient?.play(Ja5.text.toString())
-            SoundCard().btnclick(Ja5.text.toString())
+           // ttsClient?.play(Ja5.text.toString())
+
         }
         Ja6.setOnClickListener {
             Log.d("button", Ja6.text.toString())
-            ttsClient?.play(Ja6.text.toString())
-            SoundCard().btnclick(Ja6.text.toString())
+         //   ttsClient?.play(Ja6.text.toString())
+
         }
         Ja7.setOnClickListener {
             Log.d("button", Ja7.text.toString())
-            ttsClient?.play(Ja7.text.toString())
-            SoundCard().btnclick(Ja7.text.toString())
+          //  ttsClient?.play(Ja7.text.toString())
+
         }
         Ja8.setOnClickListener {
             Log.d("button", Ja8.text.toString())
-            ttsClient?.play(Ja8.text.toString())
-            SoundCard().btnclick(Ja8.text.toString())
+          //  ttsClient?.play(Ja8.text.toString())
+
         }
         Ja9.setOnClickListener {
             Log.d("button", Ja9.text.toString())
-            ttsClient?.play(Ja9.text.toString())
-            SoundCard().btnclick(Ja9.text.toString())
+        //   ttsClient?.play(Ja9.text.toString())
+
         }
         Ja10.setOnClickListener {
             Log.d("button", Ja10.text.toString())
-            ttsClient?.play(Ja10.text.toString())
-            SoundCard().btnclick(Ja10.text.toString())
+       //     ttsClient?.play(Ja10.text.toString())
+
         }
 
         return view
     }
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        when (requestCode) {
-            NETWORK_STATE_CODE -> {
-                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(App.instance.context(), "Permission Denied", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(App.instance.context(), "Permission Granted", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        TextToSpeechManager.getInstance().finalizeLibrary()
-    }
 }
--- app/src/main/java/com/example/soundhangul/SoundCard.kt
+++ app/src/main/java/com/example/soundhangul/SoundCard.kt
@@ -16,42 +16,10 @@ import kotlinx.android.synthetic.main.activity_sound_card.tabs
 
 public class SoundCard : AppCompatActivity(){
     private val consonantList = arrayOf("ㄱ", "ㄴ", "ㄷ", "ㄹ", "ㅁ", "ㅂ", "ㅅ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ")
-    //for tts
-    val TAG = "Kakao"
-    var ttsClient : TextToSpeechClient? = null
-    val NETWORK_STATE_CODE = 0
-
-    fun btnclick(str: String){
-        Log.d("tts", str)
-        ttsClient?.play(str)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sound_card)
-
-        SpeechRecognizerManager.getInstance().initializeLibrary(this)
-        TextToSpeechManager.getInstance().initializeLibrary(this)
-        ttsClient = TextToSpeechClient.Builder()
-            .setSpeechMode(TextToSpeechClient.NEWTONE_TALK_1)     // 음성합성방식
-            .setSpeechSpeed(1.0)            // 발음 속도(0.5~4.0)
-            .setSpeechVoice(TextToSpeechClient.VOICE_WOMAN_READ_CALM)  //TTS 음색 모드 설정(여성 차분한 낭독체)
-            .setListener(object : TextToSpeechListener {
-                //아래 두개의 메소드만 구현해 주면 된다. 음성합성이 종료될 때 호출된다.
-                override fun onFinished() {
-                    val intSentSize = ttsClient?.getSentDataSize()      //세션 중에 전송한 데이터 사이즈
-                    val intRecvSize = ttsClient?.getReceivedDataSize()  //세션 중에 전송받은 데이터 사이즈
-
-                    val strInacctiveText = "handleFinished() SentSize : $intSentSize  RecvSize : $intRecvSize"
-
-                    Log.i(TAG, strInacctiveText)
-                }
-
-                override fun onError(code: Int, message: String?) {
-                    Log.d(TAG, code.toString())
-                }
-            })
-            .build()
 
         val adapter = ConsonantPagerAdapter(supportFragmentManager)
         view_pager.adapter = adapter
