a43c20c58c3d48aba9d44a33e0cef59c431c4f41
cristalcho <cristalcho@unist.ac.kr>
2020-03-01 20:22:12 +0900

    upgrade makeWord

---
+++ app/src/main/java/com/example/soundhangul/GaNuFrag.kt
@@ -0,0 +1,41 @@
+package com.example.soundhangul
+
+
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import java.util.*
+
+class GaNuFrag : Fragment() {
+    var tts: TextToSpeech? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view: View? =  inflater.inflate(R.layout.fragment_ganu_frag, container, false)
+
+
+
+        tts = TextToSpeech(activity, TextToSpeech.OnInitListener{
+            if(it == TextToSpeech.SUCCESS) {
+                val result: Int = tts!!.setLanguage(Locale.KOREA)
+                if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    Toast.makeText(activity, "이 언어는 지원하지 않습니다.", Toast.LENGTH_SHORT).show()
+                } else {
+                    tts!!.setPitch(0.7f)
+                    tts!!.setSpeechRate(1.2f)
+                }
+            }
+        })
+
+
+        return view
+    }
+
+}
